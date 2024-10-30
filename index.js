dad0a2ad0beb66f106b7e9c7510776ee344fb1aa
Jaidon Clinton <jaidcoding@gmail.com>
2024-10-30 15:16:35 -0500

    Refactor prompts to use lowercase names and update license handling in README generation

--- index.js
+++ index.js
@@ -1,69 +1,66 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown.js');
-const { type } = require('os');
+import inquirer from 'inquirer';
+import fs from 'fs';
+import generateMarkdown from './utils/generateMarkdown.js';
 
 console.log('Starting the application...');
 
 const questions = [
   {
     type: 'input',
-    name: 'Title',
+    name: 'title',
     message: 'What is the title of your project?',
   },
   {
     type: 'input',
-    name: 'Description',
-    message: 'Provide a short description explaining the what, why, and how of your project. ',
+    name: 'description',
+    message: 'Provide a short description explaining the what, why, and how of your project.',
   },
   {
     type: 'input',
-    name: 'Installation',
+    name: 'installation',
     message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
   },
   {
     type: 'input',
-    name: 'Usage',
+    name: 'usage',
     message: 'Provide instructions and examples for use. Include screenshots as needed. To add a screenshot, create an assets/images folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:',
   },
   {
     type: 'input',
-    name: 'Credits',
+    name: 'credits',
     message: 'List your collaborators, if any, with links to their GitHub profiles.',
   },
   {
     type: 'list',
-    name: 'License',
+    name: 'license',
     message: 'Choose a license for your project:',
     choices: ['MIT', 'GPLv3', 'Apache 2.0', 'BSD 3-Clause', 'None'],
   },
   {
     type: 'input',
-    name: 'Badges',
+    name: 'badges',
     message: 'If you used a badge, provide the link here.',
   },
   {
     type: 'input',
-    name: 'Features',
+    name: 'features',
     message: 'If your project has a lot of features, list them here.',
   },
   {
     type: 'input',
-    name: 'How to Contribute',
+    name: 'howToContribute',
     message: 'If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so.',
   },
   {
     type: 'input',
-    name: 'Tests',
+    name: 'tests',
     message: 'Go the extra mile and write tests for your application. Then provide examples on how to run them.',
   },
   {
     type: 'input',
-    name: 'Questions',
-    message: 'If you have any questions, please feel free to reach out to me at [GitHub](
+    name: 'questions',
+    message: 'If you have any questions, please feel free to reach out to me at GitHub',
   }
-
-  // ...add more questions as needed...
 ];
 
 function writeToFile(fileName, data) {
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,21 +1,21 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (!license) return '';
+  if (!license || license === 'None') return '';
   return `![License](https://img.shields.io/badge/license-${license}-blue.svg)`;
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (!license) return '';
+  if (!license || license === 'None') return '';
   return `[License](#license)`;
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (!license) return '';
+  if (!license || license === 'None') return '';
   return `## License
 
 This project is licensed under the ${license} license.`;
@@ -31,9 +31,41 @@ ${renderLicenseBadge(data.license)}
 
 ${data.description}
 
-// ...existing code...
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Credits
+
+${data.credits}
+
+## License
 
 ${renderLicenseSection(data.license)}
+
+## Badges
+
+${data.badges}
+
+## Features
+
+${data.features}
+
+## How to Contribute
+
+${data.howToContribute}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+${data.questions}
 `;
 }
 
